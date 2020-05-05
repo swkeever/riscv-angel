b4d055c5cc18f7af43456f359c82e08f41e66a04
Zorrow2008 <yokesh98@gmail.com>
2020-05-05 15:40:32 -0700

    rendering each feature in index.js

--- src/components/Registers.js
+++ src/components/Registers.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import Category from './Category';
+import ReactDOM from 'react-dom';
+// import PropTypes from 'prop-types';
 import RegMenu from './RegMenu';
-// import {
-//   Pointers,
-//   Temporary,
-//   CalleeSaved,
-//   Arguments,
-// } from '../utils/constants';
+import useCPU from '../hooks/use-cpu';
+
 
 import getRegisters from '../utils/registers';
 
-const RegisterPanel = ({ registers }) => {
+const RegisterPanel = () => {
+  const cpu = useCPU();
+
+  if (!cpu) {
+    return <h1>Loading...</h1>;
+  }
+
   const allRegisters = getRegisters();
 
-  const registersAppended = registers.map((reg, i) => (
+  const registersAppended = cpu.registers.map((reg, i) => (
     { ...allRegisters[i], value: reg.low_ }
   ));
 
@@ -23,11 +25,13 @@ const RegisterPanel = ({ registers }) => {
   );
 };
 
-RegisterPanel.propTypes = {
-  registers: PropTypes.arrayOf(PropTypes.shape({
-    low_: PropTypes.number,
-    high_: PropTypes.number,
-  })).isRequired,
-};
+// RegisterPanel.propTypes = {
+//   registers: PropTypes.arrayOf(PropTypes.shape({
+//     low_: PropTypes.number,
+//     high_: PropTypes.number,
+//   })).isRequired,
+// };
+
+ReactDOM.render(<RegisterPanel />, document.querySelector('#registerPanel'));
 
 export default RegisterPanel;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import useCPU from './hooks/use-cpu';
 import RegisterPanel from './components/Registers';
 
 
-const App = () => {
-  const cpu = useCPU();
+// const App = () => {
+//   const cpu = useCPU();
 
-  if (!cpu) {
-    return <h1>Loading...</h1>;
-  }
+//   if (!cpu) {
+//     return <h1>Loading...</h1>;
+//   }
 
-  console.log(cpu);
+//   console.log(cpu);
 
-  return (
-    <>
-      <h1>hello world</h1>
-      <RegisterPanel registers={cpu.registers} />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h1>hello world</h1>
+//       <RegisterPanel registers={cpu.registers} />
+//     </>
+//   );
+// };
 
-ReactDOM.render(<App />, document.querySelector('#react'));
+ReactDOM.render(<RegisterPanel />, document.querySelector('#registerPanel'));
