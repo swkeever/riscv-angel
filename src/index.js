b65fa460b12a592c21a75aa0443532d0c7c8ab90
Sean Keever <swkeever@gmail.com>
2020-05-12 10:19:11 -0700

    removes commented out code

--- src/index.js
+++ src/index.js
@@ -3,22 +3,5 @@ import ReactDOM from 'react-dom';
 import RegisterPanel from './components/RegisterPanel';
 import InstructionDisplay from './components/InstructionDisplay';
 
-// const App = () => {
-//   const cpu = useCPU();
-
-//   if (!cpu) {
-//     return <h1>Loading...</h1>;
-//   }
-
-//   console.log(cpu);
-
-//   return (
-//     <>
-//       <h1>hello world</h1>
-//       <RegisterPanel registers={cpu.registers} />
-//     </>
-//   );
-// };
-
 ReactDOM.render(<RegisterPanel />, document.querySelector('#registerPanel'));
 ReactDOM.render(<InstructionDisplay />, document.querySelector('#instructionDisplay'));
