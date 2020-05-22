48c21746aa98a9e010f8417ebd5c71413bbaef8d
Sean Keever <swkeever@gmail.com>
2020-05-22 10:09:37 -0700

    test commit

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RegisterPanel from './components/RegisterPanel';
 import InstructionPanel from './components/InstructionPanel';
-import MemoryPanel from './components/MemoryPanel'
+import MemoryPanel from './components/MemoryPanel';
 
-
-
-
-
-
-
-
-
-console.log('hi')
+console.log('hi');
 
 ReactDOM.render(<RegisterPanel />, document.querySelector('#registerPanel'));
-ReactDOM.render(<InstructionPanel />, document.querySelector('#instructionPanel'));
+ReactDOM.render(
+  <InstructionPanel />,
+  document.querySelector('#instructionPanel'),
+);
 ReactDOM.render(<MemoryPanel />, document.querySelector('#memoryPanel'));
