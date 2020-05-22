e8eba2cd63cef4f5e15204aa85f5f84b56b5aff1
Sean Keever <swkeever@gmail.com>
2020-05-22 13:13:12 -0700

    updates instructions to use chart.js

--- src/components/MemoryPanel.js
+++ src/components/MemoryPanel.js
@@ -5,37 +5,27 @@ import Loader from './Loader';
 import styles from '../utils/style';
 import ModuleHeader from './ModuleHeader';
 
-Chart.defaults.global.defaultFontFamily = styles.font;
-Chart.defaults.global.defaultFontColor = styles.colorG5;
-Chart.defaults.global.defaultFontSize = parseInt(styles.sz1, 10);
-
-/* this line chart graphs the last 10 data points we've seen,
-and updates the lower and upper bound of the range to be equal
-to the min and max of this dataset, respectively. */
-const numData = 5;
-
-// seen from a console.log(cpu.totalMemory)
-// const initialMemoryTotal = 2621440;
-
-
 const MemoryPanel = () => {
   const [numArray, setNumArray] = useState([]);
-  // const [min, setMin] = useState(0);
-  // const [max, setMax] = useState(initialMemoryTotal);
   const [lastSeenData, setLastSeenData] = useState([0]);
   const cpu = useCPU();
 
   const chartRef = React.createRef();
   const [lineGraph, setLineGraph] = useState(null);
 
+  /* this line chart graphs the last 10 data points we've seen,
+     and updates the lower and upper bound of the range to be equal
+     to the min and max of this dataset, respectively. */
+  const numData = 5;
+
   const graphData = {
     labels: ['5', '4', '3', '2', '1'],
     datasets: [
       {
         fill: false, // fill area under line
-        borderColor: styles.colorP4, // line color
-        pointBackgroundColor: styles.colorP4, // point color
-        hoverBackgroundColor: styles.colorP3,
+        borderColor: styles.color.p4, // line color
+        pointBackgroundColor: styles.color.p4, // point color
+        hoverBackgroundColor: styles.color.p3,
         data: numArray,
       },
     ],
@@ -51,7 +41,6 @@ const MemoryPanel = () => {
           data: graphData,
           options: {
             responsive: true,
-            aspectRatio: 3 / 2,
             legend: {
               display: false,
             },
@@ -59,14 +48,14 @@ const MemoryPanel = () => {
               xAxes: [
                 {
                   gridLines: {
-                    color: styles.colorG8,
+                    color: styles.color.g8,
                   },
                 },
               ],
               yAxes: [
                 {
                   gridLines: {
-                    color: styles.colorG8,
+                    color: styles.color.g8,
                   },
                 },
               ],
@@ -82,15 +71,10 @@ const MemoryPanel = () => {
     }
   }, [cpu, lineGraph, chartRef, graphData]);
 
-  // const setRange = (newArr) => {
-  //   setMin(Math.min(...newArr));
-  //   setMax(Math.max(...newArr));
-  // };
-
   useEffect(() => {
     if (cpu && lastSeenData !== cpu.nonzeroMemoryTotal) {
       if (numArray.length === numData) {
-        const temp = numArray.filter(((ele, i) => i !== 0));
+        const temp = numArray.filter((ele, i) => i !== 0);
         const newArr = temp.concat(cpu.nonzeroMemoryTotal);
         // setRange(newArr);
         setNumArray(newArr);
@@ -110,15 +94,11 @@ const MemoryPanel = () => {
   return (
     <>
       <ModuleHeader title="Number of Non-zero Words" />
-      <div className="mem-graph">
-        <canvas
-          id="myChart"
-          ref={chartRef}
-        />
+      <div className="canvas-container">
+        <canvas id="myChart" ref={chartRef} />
       </div>
     </>
   );
 };
-
 
 export default MemoryPanel;
