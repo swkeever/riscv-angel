46528e7d43579c012838ea64d5fd66b8fa85902f
Sean Keever <swkeever@gmail.com>
2020-05-22 10:00:18 -0700

    tries precommit hook

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RegisterPanel from './components/RegisterPanel';
 import InstructionPanel from './components/InstructionPanel';
-import MemoryPanel from './components/MemoryPanel';
+import MemoryPanel from './components/MemoryPanel'
 
 ReactDOM.render(<RegisterPanel />, document.querySelector('#registerPanel'));
 ReactDOM.render(<InstructionPanel />, document.querySelector('#instructionPanel'));
